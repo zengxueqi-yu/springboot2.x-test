d9accdc5829b264e26839c1b993818445a65e116
曾雪琪 <cengxueqi@cengxueqideiMac-Pro.local>
2020-03-24 14:24:18 +0800

    新增接口文档测试用例

--- jpa/src/main/java/com/test/jpa/controller/UserInfoController.java
+++ jpa/src/main/java/com/test/jpa/controller/UserInfoController.java
@@ -2,13 +2,16 @@ package com.test.jpa.controller;
 
 import com.test.jpa.entity.Userinfo;
 import com.test.jpa.service.UserInfoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+/**
+ * 用户控制器信息
+ * @author zengxueqi
+ * @since 2020/3/24
+ */
 @RestController
 @RequestMapping("/api")
 public class UserInfoController {
@@ -16,8 +19,15 @@ public class UserInfoController {
     @Resource
     private UserInfoService userInfoService;
 
-    @GetMapping("/getUserInfos")
-    public List<Userinfo> getUserInfos(){
+    /**
+     * 获取用户信息
+     * @param userinfo
+     * @return java.util.List<com.test.jpa.entity.Userinfo>
+     * @author zengxueqi
+     * @since 2020/3/24
+     */
+    @PostMapping("/getUserInfos")
+    public List<Userinfo> getUserInfos(@RequestBody Userinfo userinfo){
         return userInfoService.findByUsername("lisheng");
     }
 
